d8ecbdec44dfd2d58fdc68268e7dcc17b9c78b36
Ananya6622 <ananyaetti6622@gmail.com>
2023-09-29 18:47:28 +0530

    added

---
+++ WithoutLambda1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaExpressions
+{
+    class WithoutLambda1
+    {
+        static List<int> CalculateSquares(List<int> numbers)
+        {
+            List<int> squares = new List<int>();
+            foreach (var value in numbers)
+            {
+                squares.Add(value * value);
+            }
+            return squares;
+        }
+
+        static List<int> FindDivisibleBy3(List<int> numbers)
+        {
+            List<int> divBy3 = new List<int>();
+            foreach (var value in numbers)
+            {
+                if (value % 3 == 0)
+                {
+                    divBy3.Add(value);
+                }
+            }
+            return divBy3;
+        }
+
+        public static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 36, 71, 12, 15, 29, 18, 27, 17, 9, 34 };
+
+            Console.Write("The list : ");
+            foreach (var value in numbers)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+
+            List<int> squares = CalculateSquares(numbers);
+
+            Console.Write("Squares : ");
+            foreach (var value in squares)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+
+            List<int> divBy3 = FindDivisibleBy3(numbers);
+
+            Console.Write("Numbers Divisible by 3 : ");
+            foreach (var value in divBy3)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ WithoutLambda2.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//class Student
+//{
+//    public int rollNo { get; set; }
+//    public string name { get; set; }
+//}
+
+//namespace LambdaExpressions
+//{
+//    public class WithoutLambda2
+//    {
+//        static List<Student> SortStudentsByName(List<Student> details)
+//        {
+//            details.Sort((x, y) => x.name.CompareTo(y.name));
+//            return details;
+//        }
+
+//        ////public static void Main(string[] args)
+//        {
+//            List<Student> details = new List<Student>() {
+//                new Student { rollNo = 1, name = "Liza" },
+//                new Student { rollNo = 2, name = "Stewart" },
+//                new Student { rollNo = 3, name = "Tina" },
+//                new Student { rollNo = 4, name = "Stefani" },
+//                new Student { rollNo = 5, name = "Trish" }
+//            };
+
+//            List<Student> sortedDetails = SortStudentsByName(details);
+
+//            foreach (var value in sortedDetails)
+//            {
+//                Console.WriteLine(value.rollNo + " " + value.name);
+//            }
+//        }
+//    }
+//}
